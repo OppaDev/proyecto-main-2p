0cf02715c56a35c47cee83bdea9b22090ae7bbb2
Cristian Acalo <cristianchoacalo@gmail.com>
2025-07-23 14:08:21 -0500

    Fixing errors with JjwUtils

--- ms-autorizacion/src/main/java/ec/edu/espe/ms_autorizacion/payload/AuthenticationRequest.java
+++ ms-autorizacion/src/main/java/ec/edu/espe/ms_autorizacion/payload/AuthenticationRequest.java
@@ -1,4 +1,12 @@
 package ec.edu.espe.ms_autorizacion.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class AuthenticationRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
 }
--- ms-autorizacion/src/main/java/ec/edu/espe/ms_autorizacion/payload/AuthenticationResponse.java
+++ ms-autorizacion/src/main/java/ec/edu/espe/ms_autorizacion/payload/AuthenticationResponse.java
@@ -1,4 +1,10 @@
 package ec.edu.espe.ms_autorizacion.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthenticationResponse {
+    private String token;
 }
